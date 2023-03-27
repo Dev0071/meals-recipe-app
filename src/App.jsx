e91b34788c9455a6d42ebce1a17a7e4b5999cb65
Dev0071 <lecturergacheru71@gmail.com>
2023-03-27 18:28:20 +0300

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React from "react";
+import Search from "./components/Search";
+import Meals from "./components/Meals";
+import Modal from "./components/Modal";
+import Favourites from "./components/Favourites";
+
 const App = () => {
-  return <div className='App'>hello world</div>;
+  return (
+    <>
+      {/* <Search /> */}
+      {/* <Favourites /> */}
+      <Meals />
+      {/* <Modal /> */}
+    </>
+  );
 };
 
 export default App;
