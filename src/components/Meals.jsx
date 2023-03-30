14cd50db15c1bb7daea3c637943a799705860da6
Dev0071 <lecturergacheru71@gmail.com>
2023-03-31 02:08:20 +0300

    fetched data from mealsDb Api

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,110 +2,36 @@ import React from 'react';
 // import { AppContext } from '../context';
 // import { useContext } from 'react';
 import { useGlobalContext } from '../context';
+import Loader from './Loader';
 
 function Meals() {
-  const context = useGlobalContext();
-  console.log(context);
+  const { isLoading, meals } = useGlobalContext();
+  const defaultImg =
+    '"https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"';
+  console.log(meals);
+  if (isLoading) return <Loader />;
+
   return (
     <section className="bg-gray-100  ">
-      <div className="flex  gap-[1.5rem] px-[1.5rem] flex-wrap py-6 md:px-[5rem] md:gap-[2rem] md:py-8">
-        <div className="flex flex-col h-[19rem]   rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover grow-0 ">
-            <img
-              className=" "
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-
-        <div className="flex flex-col h-[19rem]   rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover grow-0 ">
-            <img
-              className=" "
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-
-        <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover grow-0 ">
-            <img
-              className=" "
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-
-        <div className="flex flex-col h-[19rem] rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover grow-0 ">
-            <img
-              className=" "
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-
-        <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover grow-0 ">
-            <img
-              className=" "
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-
-        <div className="flex flex-col h-[19rem] rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover grow-0 ">
-            <img
-              className=" "
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-        <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover grow-0 ">
-            <img
-              className=" "
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-      </div>
+      {meals.map((meal) => {
+        const { strmeal: name, idMeal: id, strMealThumb: image } = meal;
+        return (
+          <article
+            key={id}
+            className="flex  gap-[1.5rem] px-[1.5rem] flex-wrap py-6 md:px-[5rem] md:gap-[2rem] md:py-8"
+          >
+            <div className="flex flex-col h-[19rem]   rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+              <div className="object-cover grow-0 ">
+                <img className=" " src={image} alt="food" />
+              </div>
+              <div className="flex justify-between py-6 px-6 bg-white">
+                <p>{name}</p>
+                icon
+              </div>
+            </div>
+          </article>
+        );
+      })}
     </section>
   );
 }
--- src/context.jsx
+++ src/context.jsx
@@ -1,9 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
 
+const allmealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const randomMeal = 'https://www.themealdb.com/api/json/v1/1/random.php';
 export const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchMeal = async (url) => {
+    try {
+      const data = await axios.get(url);
+      // console.log(data?.data.meals);
+      setMeals(data?.data.meals);
+      setIsLoading(false);
+    } catch (e) {
+      console.log(e.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchMeal(allmealsUrl);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ meals, isLoading }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
