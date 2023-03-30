58aa0d87e787b31ef1927a653b844ae665942aaf
Dev0071 <lecturergacheru71@gmail.com>
2023-03-30 20:55:20 +0300

    implemented  responsiveness

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -8,10 +8,81 @@ function Meals() {
   console.log(context);
   return (
     <section className="bg-gray-100  ">
-      <div className="flex flex-wrap py-8 px-[6rem] gap-[3rem] ">
-        <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover">
+      <div className="flex  gap-[1.5rem] px-[1.5rem] flex-wrap py-6 md:px-[5rem] md:gap-[2rem] md:py-8">
+        <div className="flex flex-col h-[19rem]   rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover grow-0 ">
             <img
+              className=" "
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-6 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+
+        <div className="flex flex-col h-[19rem]   rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover grow-0 ">
+            <img
+              className=" "
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-6 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+
+        <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover grow-0 ">
+            <img
+              className=" "
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-6 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+
+        <div className="flex flex-col h-[19rem] rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover grow-0 ">
+            <img
+              className=" "
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-6 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+
+        <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover grow-0 ">
+            <img
+              className=" "
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-6 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+
+        <div className="flex flex-col h-[19rem] rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover grow-0 ">
+            <img
+              className=" "
               src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
               alt="food"
             />
@@ -22,56 +93,9 @@ function Meals() {
           </div>
         </div>
         <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover">
+          <div className="object-cover grow-0 ">
             <img
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-        <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover">
-            <img
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-        <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover">
-            <img
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-        <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover">
-            <img
-              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt="food"
-            />
-          </div>
-          <div className="flex justify-between py-6 px-6 bg-white">
-            <p>meals name</p>
-            icon
-          </div>
-        </div>
-        <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
-          <div className="object-cover">
-            <img
+              className=" "
               src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
               alt="food"
             />
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Search = () => {
   return (
-    <section className="px-[4rem] py-[2rem] flex gap-[2rem]">
+    <section className="px-[1rem] py-[1rem] flex gap-[2rem]  md:px-[4rem] md:py-[2rem]">
       <input
         type="text"
         placeholder="type favourite meal"
-        className=" rounded-sm bg-gray-100 px-2 py-1 text-sm w-[14rem]"
+        className=" rounded-sm bg-gray-100 px-2 py-1 text-sm md:w-[14rem]"
       />
       <button
         type="submit"
