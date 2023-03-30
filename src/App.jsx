e8650fba39040f90bb92b7d581b9de65ad42d012
Dev0071 <lecturergacheru71@gmail.com>
2023-03-30 14:41:40 +0300

    structure of the search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Search from "./components/Search";
-import Meals from "./components/Meals";
-import Modal from "./components/Modal";
-import Favourites from "./components/Favourites";
+import React from 'react';
+import Search from './components/Search';
+import Meals from './components/Meals';
+import Modal from './components/Modal';
+import Favourites from './components/Favourites';
 
 const App = () => {
   return (
     <>
-      {/* <Search /> */}
+      <Search />
       {/* <Favourites /> */}
       <Meals />
       {/* <Modal /> */}
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React from 'react';
 
 const Search = () => {
-  return <div>search</div>;
+  return (
+    <div className="px-[4rem] py-[2rem] flex gap-[2rem]">
+      <input
+        type="text"
+        placeholder="type favourite meal"
+        className=" rounded-sm bg-gray-100 px-2 py-1 text-sm w-[14rem]"
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 text-sm text-white px-2 py-1 rounded-sm"
+      >
+        Search
+      </button>
+      <button
+        type="submit"
+        className="bg-blue-200 text-sm  px-2 py-1 rounded-sm font-medium"
+      >
+        Surprise me
+      </button>
+    </div>
+  );
 };
 
 export default Search;
