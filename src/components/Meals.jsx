39ca6ae72f03a0d83e9013b53c7b74c3a0e1ff55
Dev0071 <lecturergacheru71@gmail.com>
2023-03-30 19:44:53 +0300

    fixed bugs

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+// import { AppContext } from '../context';
+// import { useContext } from 'react';
+import { useGlobalContext } from '../context';
 
 function Meals() {
+  const context = useGlobalContext();
+  console.log(context);
   return (
     <section className="bg-gray-100  ">
-      <div className="flex flex-wrap py-8 px-[6rem] gap-[3rem]">
+      <div className="flex flex-wrap py-8 px-[6rem] gap-[3rem] ">
         <div className="flex flex-col h-[19rem]  rounded-lg overflow-hidden shadow-lg w-[22rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
           <div className="object-cover">
             <img
