9e66acc0cf725549197d3c1ca9ce6ae064cda63a
Dev0071 <lecturergacheru71@gmail.com>
2023-03-30 12:26:40 +0300

    structure of home page

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,7 +1,100 @@
-import React from "react";
+import React from 'react';
 
 function Meals() {
-  return <div>Meals</div>;
+  return (
+    <section className="bg-gray-100">
+      <div className="flex p-12 w-screen">
+        <div className="flex flex-col rounded-lg overflow-hidden shadow-lg w-[23rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover">
+            <img
+              className="h-64"
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-3 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap p-12">
+        <div className="flex flex-col rounded-lg overflow-hidden shadow-lg w-[23rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover">
+            <img
+              className="h-64"
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-3 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap p-12">
+        <div className="flex flex-col rounded-lg overflow-hidden shadow-lg w-[23rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover">
+            <img
+              className="h-64"
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-3 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap p-12">
+        <div className="flex flex-col rounded-lg overflow-hidden shadow-lg w-[23rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover">
+            <img
+              className="h-64"
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-3 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap p-12">
+        <div className="flex flex-col rounded-lg overflow-hidden shadow-lg w-[23rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover">
+            <img
+              className="h-64"
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-3 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-wrap p-12">
+        <div className="flex flex-col rounded-lg overflow-hidden shadow-lg w-[23rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out">
+          <div className="object-cover">
+            <img
+              className="h-64"
+              src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              alt="food"
+            />
+          </div>
+          <div className="flex justify-between py-3 px-6 bg-white">
+            <p>meals name</p>
+            icon
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Meals;
