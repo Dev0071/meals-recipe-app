ce6c800d71af505fc277f74fe30e921937f95b4d
Dev0071 <lecturergacheru71@gmail.com>
2023-04-05 01:18:46 +0300

    designed and showed modal on card click

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React from 'react';
+import { useGlobalContext } from '../context';
 
 const Modal = () => {
-  return <div>Modal</div>;
+  const { selectedMeal, closeModal } = useGlobalContext();
+  const {
+    strMealThumb: image,
+    strMeal: title,
+    strInstructions: text,
+    strSource: source,
+  } = selectedMeal;
+
+  return (
+    <aside className="modal-overlay">
+      <div className="modal-container bg-white">
+        <img
+          src={image}
+          className="h-[10rem] w-full  object-cover"
+          alt={title}
+        />
+        <div className="flex p-8 flex-col">
+          <h4 className="text-2xl">{title}</h4>
+          <p className="mt-3 mb-2 text-gray-600 text-lg">Cooking Intruction</p>
+          <p className="leading-loose text-gray-500 mb-2">{text}</p>
+          <a href={source} target="_blank" className="text-blue-500 underline">
+            Original Source
+          </a>
+          <button
+            className="text-white bg-red-700 py-2 rounded-lg mt-6"
+            onClick={closeModal}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default Modal;
