f728146eead78ee7b8dfa89a4ccd12d2aad71aae
Dev0071 <lecturergacheru71@gmail.com>
2023-04-06 23:06:57 +0300

    created the favourite structure

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import { useGlobalContext } from '../context';
 
 const Favourites = () => {
+  const [favourites, removeFromFavorites] = useGlobalContext();
   return (
     <section className="bg-black text-white py-[1rem] px-[4rem]">
       <h2>Favourites</h2>
-      <div className="mt-[1.5rem]">
-        <div className="h-17 w-20 rounded-full overflow-hidden ring-4 ">
-          <img
-            src="https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-            alt="food-image"
-          />
-        </div>
-        <button className="ml-[1rem] mt-3  text-sm transition duration-700 ease-in-out hover:text-red-500">
-          remove
-        </button>
-      </div>
+      {favourites.map((meal) => {
+        const { idMeal: id, strMealThumb: image } = meal;
+        return (
+          <article key={id} className="mt-[1.5rem]">
+            <div className="h-17 w-20 rounded-full overflow-hidden ring-4 ">
+              <img src={image} alt="food-image" />
+            </div>
+            <button
+              className="ml-[1rem] mt-3  text-sm transition duration-700 ease-in-out hover:text-red-500"
+              onClick={() => removeFromFavorites(id)}
+            >
+              remove
+            </button>
+          </article>
+        );
+      })}
     </section>
   );
 };
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 // import { AppContext } from '../context';
 // import { useContext } from 'react';
+import { AiOutlineLike } from 'react-icons/ai';
 import { useGlobalContext } from '../context';
 import Loader from './Loader';
 
 function Meals() {
-  const { isLoading, meals, selectMeal } = useGlobalContext();
+  const { isLoading, meals, selectMeal, addToFavourite } = useGlobalContext();
   const defaultImg =
     '"https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWVhbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"';
   // console.log(meals);
@@ -24,14 +25,19 @@ function Meals() {
             <article
               key={id}
               className="flex flex-col h-[19rem]   rounded-lg overflow-hidden shadow-lg w-[21rem] cursor-pointer hover:skew-x-2 transition duration-700 ease-in-out"
-              onClick={() => selectMeal(id)}
             >
-              <img className="h-[16rem]" src={image} alt="food" />
-
+              <img
+                className="h-[16rem]"
+                src={image}
+                alt="food"
+                onClick={() => selectMeal(id)}
+              />
               <div className="h-12 bg-white">
-                <div className="flex px-5 justify-between py-3 ">
+                <div className="flex px-5 justify-items-center justify-between py-3 ">
                   <p>{name}</p>
-                  <p>icon</p>
+                  <button onClick={() => addToFavourite(id)}>
+                    <AiOutlineLike />
+                  </button>
                 </div>
               </div>
             </article>
